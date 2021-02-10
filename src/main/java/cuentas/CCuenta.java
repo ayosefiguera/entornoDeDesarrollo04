5ba7ad8e8d81b4da52b87144fe3da2af54cd67c8
ayosefiguera <ayo.afa@gmail.com>
2021-02-10 17:24:24 +0000

    Añadido JavaDoc y cambios en código

--- src/main/java/cuentas/CCuenta.java
+++ src/main/java/cuentas/CCuenta.java
@@ -1,58 +1,75 @@
 package cuentas;
 
+/**
+ * Esta clase crea una cuenta bancaria básica para el usuario. Controla el sueldo
+ * el ingreso y la retirada de efectivo.
+ * 
+ * @author Ayose Figuera Alfonso
+ * @version 0.5
+ * 
+ */
+
 public class CCuenta {
 
     /**
-     * @return the nombre
+     * Retorna el nombre de usuario de la cuenta.
+     * @return nombre.
      */
     public String getNombre() {
         return nombre;
     }
 
     /**
-     * @param nombre the nombre to set
+     * Método que permite cambiar el nombre del usuario.
+     * @param nombre valor del nombre que deseamos modificar.
      */
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
 
     /**
-     * @return the cuenta
+     * Retorna el numero de la cuenta del usuario.
+     * @return número de cuenta.
      */
     public String getCuenta() {
         return cuenta;
     }
 
     /**
-     * @param cuenta the cuenta to set
+     * Método que permite modificar el número de cuenta del usuario.
+     * @param cuenta número nueva cuenta.
      */
     public void setCuenta(String cuenta) {
         this.cuenta = cuenta;
     }
 
     /**
-     * @return the saldo
+     * Retorna el valor del saldo actual en la cuenta del usuario.
+     * @return saldo de la cuenta
      */
     public double getSaldo() {
         return saldo;
     }
 
     /**
-     * @param saldo the saldo to set
+     * Método que permite cambiar el valor del saldo
+     * @param saldo nuevo valor del saldo.
      */
     public void setSaldo(double saldo) {
         this.saldo = saldo;
     }
 
     /**
-     * @return the tipoInterés
+     * Retorna el valor del tipo de interés de la cuenta.
+     * @return valor del tipo de interés.
      */
     public double getTipoInterés() {
         return tipoInterés;
     }
 
     /**
-     * @param tipoInterés the tipoInterés to set
+     * Método que permite cambiar el tipo de interés de la cuenta bancaria.
+     * @param tipoInterés nuevo valor del tipo de interés
      */
     public void setTipoInterés(double tipoInterés) {
         this.tipoInterés = tipoInterés;
@@ -64,10 +81,21 @@ public class CCuenta {
     private double saldo;
     private double tipoInterés;
 
+    /**
+     * Contructor básico de la cuenta.
+     */
     public CCuenta()
     {
     }
 
+    /**
+     * Contructor de la clase.
+     * @param nom nombre del usuario.
+     * @param cue numero de cuenta.
+     * @param sal saldo actual en la cuenta.
+     * @param tipo tipo de interes de la cuenta bancaria.
+     */
+    
     public CCuenta(String nom, String cue, double sal, double tipo)
     {
         nombre =nom;
@@ -75,18 +103,36 @@ public class CCuenta {
         saldo=sal;
     }
 
+    /**
+     * Retorna el saldo actual de la cuenta.
+     * @return saldo de la cualenta.
+     */
     public double estado()
     {
         return getSaldo();
     }
-
+    
+    /**
+     * Método que permite ingresar dinero en la cuenta. Suma el valor introducido  
+     * al valor de la cuenta actual. No se permiten valores negativos.
+     * @param cantidad dinero a ingresar en la cuenta.
+     * @throws Exception Lanza un error si el valor introducido en negativo
+     */
     public void ingresar(double cantidad) throws Exception
     {
         if (cantidad<0)
             throw new Exception("No se puede ingresar una cantidad negativa");
         setSaldo(getSaldo() + cantidad);
     }
-
+    
+     /**
+     * Método que permite al usuario retirar dinero de la cuenta.
+     * El usuario introduce un valor a retirar y este se resta del saldo solo si
+     * es posible..
+     * @param cantidad dinero a retirar de la cuenta.
+     * @throws Exception Lanza un error si el valor que quire retirar el usuario es mayor 
+     * al de la cuenta. No puede retirar valores negativos.
+     */
     public void retirar(double cantidad) throws Exception
     {
         if (cantidad <= 0)
